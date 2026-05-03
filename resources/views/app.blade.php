09b5510929148e7afb94221787011a4c2e70d601
DH1252 <yusuftempmail@gmail.com>
2026-05-04 06:53:58 +0700

    optimize: self-host Font Awesome to eliminate CDN roundtrip
    - Download and self-host Font Awesome 6.5.1 CSS + woff2 webfonts
    - Remove cdnjs.cloudflare.com dependency and preconnect overhead
    - Keep non-blocking load with low fetchpriority

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -28,13 +28,10 @@
     <noscript>
         <link rel="stylesheet" href="{{ asset('fonts/public-sans.css') }}">
     </noscript>
-    {{-- Preconnect to CDN for icon font --}}
-    <link rel="dns-prefetch" href="https://cdnjs.cloudflare.com">
-    <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossorigin>
-    {{-- Non-critical icon font: low priority, non-blocking --}}
-    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" as="style" fetchpriority="low" onload="this.onload=null;this.rel='stylesheet'">
+    {{-- Self-hosted Font Awesome 6.5.1 (~360 KB woff2 + 103 KB CSS) --}}
+    <link rel="preload" href="{{ asset('fonts/font-awesome/css/all.min.css') }}" as="style" fetchpriority="low" onload="this.onload=null;this.rel='stylesheet'">
     <noscript>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        <link rel="stylesheet" href="{{ asset('fonts/font-awesome/css/all.min.css') }}">
     </noscript>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
